333f0a22b1fd9d2c8911616b6249c77159754aa1
alexandralesan1 <alexandra.lesan@isa.utm.md>
2025-05-14 13:56:55 +0300

    changed methods from filter controller to product service

--- WebApplication4/Controllers/FilterController.cs
+++ WebApplication4/Controllers/FilterController.cs
@@ -16,53 +16,28 @@ namespace WebApplication4.Controllers
             _productService = new ProductService();
         }
 
-        public ActionResult Filter(string category, string[] selectedBrands, string[] selectedCategories, string[] selectedCountries, string[] selectedSpecialPromotions)
+        public ActionResult Filter(
+            string category,
+            string[] selectedBrands,
+            string[] selectedCategories,
+            string[] selectedCountries,
+            string[] selectedSpecialPromotions)
         {
-            var products = _productService.GetAllProducts().AsQueryable();
+            var allProducts = _productService.GetFilteredProducts(
+                category,
+                selectedBrands,
+                selectedCategories,
+                selectedCountries,
+                selectedSpecialPromotions
+            );
 
-           
-            if (!string.IsNullOrEmpty(category))
-            {
-                products = products.Where(p => p.Category.ToString() == category);
-            }
-
-          
-            if (selectedBrands?.Length > 0)
-            {
-                products = products.Where(p => selectedBrands.Contains(p.Brand.ToString()));
-            }
-
-       
-            if (selectedCategories?.Length > 0)
-            {
-                products = products.Where(p => selectedCategories.Contains(p.Category.ToString()));
-            }
-
-          
-            if (selectedCountries?.Length > 0)
-            {
-                products = products.Where(p => selectedCountries.Contains(p.Country.ToString()));
-            }
-
-        
-            if (selectedSpecialPromotions?.Length > 0)
-            {
-                products = products.Where(p => selectedSpecialPromotions.Contains(p.SpecialCategory.ToString()));
-            }
-
-            
-            var allProducts = products.ToList();
-
-         
             ViewBag.BrandList = allProducts.Select(p => p.Brand.ToString()).Distinct().ToList();
             ViewBag.CategoryList = allProducts.Select(p => p.Category.ToString()).Distinct().ToList();
             ViewBag.CountryList = allProducts.Select(p => p.Country.ToString()).Distinct().ToList();
             ViewBag.SpecialPromotionList = allProducts.Select(p => p.SpecialCategory.ToString()).Distinct().ToList();
 
-          
             ViewBag.SelectedCategory = category;
 
-            
             return View(allProducts);
         }
     }
